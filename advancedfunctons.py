00f19477e76644df1dc57d3e545e02c0c8eff54d
Umer-1-14-debug <m.umer0000013@gmail.com>
2025-11-19 17:28:29 +0500

    Add files via upload

---
+++ advancedfunctons.py
@@ -0,0 +1,9 @@
+number1=[1,2,3,4,5]
+number2=[6,7,8,9,10]
+result=list(map(lambda x,y:x+y,number1,number2))
+print(result)
+num=[1,2,3,4,5]
+def number4(n):
+    return n*n
+result1=list(map(number4,num))
+print(result1)
