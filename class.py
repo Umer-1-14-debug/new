69a62b2b23c4b42c9619930c37d62779935e8071
Umer-1-14-debug <m.umer0000013@gmail.com>
2025-11-26 17:56:56 +0500

    Add files via upload

--- class.py
+++ class.py
@@ -8,3 +8,16 @@ class vehicle:
         self.milage=milage
         print("The top speed is ",maxspeed," and the milage is ",milage)
 ve=vehicle(201,19)
+class birds:
+    specie="bird"
+    def __init__(self,name,type1):
+        self.name=name
+        self.age=type1
+    def s(self):
+        print(self.specie)
+        print(self.name)
+        print(self.type1)
+bi1=birds("sparrow","b1")
+bi2=birds("crow","b2")
+bi1.s()
+bi2.s()
